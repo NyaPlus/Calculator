753c10b96eddb7fb6b3ecfcd8bbe5a9fb5391eb7
喵Plus <nyaplus@qq.com>
2016-10-09 18:51:00 +0800

    2016/10/9 - 18:50吃饭前更新

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -4,7 +4,7 @@
 #include "stdafx.h"
 #include <Windows.h>
 
-void convert_it(char *, char *);
+char convert_it(char *, char *, int, int, int);
 int calculate(char *);
 
 int main()
@@ -12,13 +12,20 @@ int main()
 	printf("输入算式（支持+加-减*乘/除()括号^幂）：\n");
 	char *c1 = new char[500], *c2 = new char[500];
 	scanf("%s", c1);
-	convert_it(c1, c2);
-	printf("计算结果：%d\n", calculate(c2));
+	char error;
+	if (!(error = convert_it(c1, c2,0,0,strlen(c1))))
+		printf("计算结果：%d\n", calculate(c2));
+	else
+		printf("检测到非法字符：%c\n", error);
 	system("pause");
     return 0;
 }
 
-void convert_it(char *from, char *to)
+char convert_it(char *from, char *to, int fromnum, int tonum, int length)
 {
+	for (int i = fromnum;i <= length;i++)
+	{
 
+	}
+	return 0;
 }
